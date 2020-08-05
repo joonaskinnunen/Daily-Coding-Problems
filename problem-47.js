54c25304f4cb169056976113ae5a5abbf0f5b49a
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-08-05 20:50:55 +0300

    Add solution to problem 47

---
+++ problem-47.js
@@ -0,0 +1,16 @@
+/* Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock once. You must buy before you can sell it.
+
+For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars. */
+
+const getMaxProfit = (arr) => {
+    let maxProfit = null
+
+    for(let i = 0; i < arr.length - 1; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[j] - arr[i] > maxProfit) maxProfit = arr[j] - arr[i]
+        }
+    }
+    return maxProfit
+}
+
+console.log(getMaxProfit([9, 11, 8, 5, 7, 10]))
